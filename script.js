57b514b14457d648c174991b361ac1126bafdd3d
Manoj <117893476+ManojAcharya27@users.noreply.github.com>
2023-07-10 15:53:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here
+let bandNames = ['The Rolling Stones', 'Pink Floyd', 'The Beatles', 'Led Zeppelin', 'Queen'];
 
+function sortAndDisplayBandNames() {
+  // Remove articles from band names and create a new array
+  let sortedBandNames = bandNames.map(name => {
+    // Remove leading articles using regular expressions
+    return name.replace(/^(a|an|the)\s/i, '');
+  });
+
+  // Sort the band names in lexicographic order
+  sortedBandNames.sort();
+
+  // Get the <ul> element with the id 'band'
+  let bandList = document.getElementById('band');
+
+  // Clear any existing content inside the <ul> element
+  bandList.innerHTML = '';
+
+  // Add each band name as an <li> element inside the <ul> element
+  sortedBandNames.forEach(name => {
+    let listItem = document.createElement('li');
+    listItem.textContent = name;
+    bandList.appendChild(listItem);
+  });
+}
